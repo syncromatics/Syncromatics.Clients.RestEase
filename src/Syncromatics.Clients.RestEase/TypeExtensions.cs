e8cd74bb015d19811bdc7a9cc5f5bb97fe79c50c
Daniel James <daniel@thzinc.com>
2019-06-11 10:42:59 -0700

    (+semver: feature) Added optional request modifier delegate parameter

--- src/Syncromatics.Clients.RestEase/TypeExtensions.cs
+++ src/Syncromatics.Clients.RestEase/TypeExtensions.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using RestEase;
 
 namespace Syncromatics.Clients.RestEase
@@ -13,11 +16,15 @@ namespace Syncromatics.Clients.RestEase
         /// </summary>
         /// <param name="rootUrl">Root URL on which to base client requests</param>
         /// <param name="responseDeserializer">Optional response deserializer implementation</param>
+        /// <param name="requestModifier">Optional delegate called on every request</param>
         /// <typeparam name="TClient">Type of the RestEase-decorated interface</typeparam>
         /// <returns>Instance of the requested RestEase client</returns>
-        public static TClient GetRestClient<TClient>(string rootUrl, ResponseDeserializer responseDeserializer = null)
+        public static TClient GetRestClient<TClient>(
+            string rootUrl,
+            ResponseDeserializer responseDeserializer = null,
+            RequestModifier requestModifier = null)
         {
-            return (TClient)typeof(TClient).GetRestClient(rootUrl, responseDeserializer);
+            return (TClient)typeof(TClient).GetRestClient(rootUrl, responseDeserializer, requestModifier);
         }
 
         /// <summary>
@@ -26,13 +33,20 @@ namespace Syncromatics.Clients.RestEase
         /// <param name="type">Type of the RestEase-decorated interface</param>
         /// <param name="rootUrl">Root URL on which to base client requests</param>
         /// <param name="responseDeserializer">Optional response deserializer implementation</param>
+        /// <param name="requestModifier">Optional delegate called on every request</param>
         /// <returns>Instance of the requested RestEase client</returns>
-        public static object GetRestClient(this Type type, string rootUrl, ResponseDeserializer responseDeserializer = null)
+        public static object GetRestClient(
+            this Type type,
+            string rootUrl,
+            ResponseDeserializer responseDeserializer = null,
+            RequestModifier requestModifier = null)
         {
-            return new RestClient(rootUrl)
+            return new RestClient(rootUrl, requestModifier ?? NoopRequestModifier)
             {
                 ResponseDeserializer = responseDeserializer ?? new BaseJsonResponseDeserializer(),
             }.For(type);
         }
+
+        private static Task NoopRequestModifier(HttpRequestMessage request, CancellationToken cancellationToken) => Task.CompletedTask;
     }
 }
